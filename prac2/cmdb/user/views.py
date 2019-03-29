1986de17f499939579a1d04edf1a284f7e8c6861
wangyuxi <2379797392@qq.com>
2019-03-29 17:43:14 +0800

    3/29

--- prac2/cmdb/user/views.py
+++ prac2/cmdb/user/views.py
@@ -1,6 +1,10 @@
 #encoding: utf-8
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+import time
+from user import user
 
 def index(request):
-    return HttpResponse('我的第一个网页')
+    users = user.load_data()
+#    return HttpResponse('我的第一个网页')dd
+    return render(request,'user/index.html',{'current_time' :time.time(),'users' : users.items()})

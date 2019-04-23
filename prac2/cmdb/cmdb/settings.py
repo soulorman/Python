812279d5922ce0443bf214a83798c22a480283fe
wangyuxi <2379797392@qq.com>
2019-04-23 17:33:06 +0800

    4/23

--- prac2/cmdb/cmdb/settings.py
+++ prac2/cmdb/cmdb/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'cmdb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static/')
+]

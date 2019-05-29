14795bab277bd8c6d3c4b6f2d90a40547f62739d
wangyuxi <2379797392@qq.com>
2019-05-29 17:49:24 +0800

    5/29

--- prac2/views.py
+++ prac2/views.py
@@ -1,5 +1,10 @@
 #enconding: utf-8
 
+import os
+import time
+
+from django.conf import settings
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -9,7 +14,8 @@ def index(request):
 def upload(request):
     log = request.FILES.get('log')
     if log:
-        print(dir(log))
-        print(log.read())
-
+        path = os.path.join(settings.BASE_DIR,'media','uploads', str(time.time()))
+        fhandler = open(path, "wb")
+        fhandler.write(log.read())
+        fhandler.close()
     return HttpResponse("upload")

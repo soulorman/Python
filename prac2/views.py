c720f534125bbdf792ceb6a7d2efebd391ef0255
wangyuxi <2379797392@qq.com>
2019-05-30 16:51:35 +0800

    5/30

--- prac2/views.py
+++ prac2/views.py
@@ -2,11 +2,15 @@
 
 import os
 import time
+import json
 
 from django.conf import settings
 
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import AccessLogFileModel
+
 
 def index(request):
     return render(request,'webanalysis/index.html')
@@ -16,6 +20,16 @@ def upload(request):
     if log:
         path = os.path.join(settings.BASE_DIR,'media','uploads', str(time.time()))
         fhandler = open(path, "wb")
-        fhandler.write(log.read())
+
+        for chunk in log.chunks():
+            fhandler.write(log.read(chunk))
         fhandler.close()
+
+        obj = AccessLogFileModel(name=log.name, path=path)
+        obj.save()
+
+        path = os.path.join(settings.BASE_DIR,'media','notices', str(time.time()))
+        with open(path,'w') as fhandler:
+            fhandler.write(json.dumps({'id' : obj.id, 'path' : obj.path}))
+
     return HttpResponse("upload")

2a9ffe386f97518f797cbe562d48cd5a6f82edaf
wangyuxi <2379797392@qq.com>
2019-06-04 18:06:15 +0800

    6/4

--- prac2/models.py
+++ prac2/models.py
@@ -1,6 +1,7 @@
 #encoding: utf-8
 from django.db import models
 
+
 class AccessLogFileModel(models.Model):
     name = models.CharField(max_length=128, null=False, default='')
     path = models.CharField(max_length=1024, null=False, default='')
--- prac2/views.py
+++ prac2/views.py
@@ -9,11 +9,14 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import AccessLogFileModel
+from .models import AccessLogFileModel,AccessLog
+from django.http import JsonResponse
+from django.db.models import Count
 
 
 def index(request):
-    return render(request,'webanalysis/index.html')
+    files = AccessLogFileModel.objects.filter(status=0).order_by('-created_time')[:10]
+    return render(request,'webanalysis/index.html', {"files" : files})
 
 def upload(request):
     log = request.FILES.get('log')
@@ -33,3 +36,27 @@ def upload(request):
             fhandler.write(json.dumps({'id' : obj.id, 'path' : obj.path}))
 
     return HttpResponse("upload")
+
+
+def dist_status_code(request):
+    objs = AccessLog.objects.values("status_code").filter(file_id=request.GET.get('id',0)).annotate(codecount=Count("status_code")).order_by('-codecount')
+
+    legend = []
+    series = []
+    for line in objs:
+        legend.append(line.get('status_code'))
+        series.append({"name" : line.get('status_code'), "value" : line.get('codecount')})
+
+    return JsonResponse({'code' : 200, 'result' : {'legend' : legend, 'series' : series}})
+
+
+def trend_visit(request):
+    objs = AccessLog.objects.values("status_code").filter(file_id=request.GET.get('id',0)).annotate(codecount=Count("status_code"))
+
+    series = []
+    xAxis = []
+    for line in objs:
+        xAxis.append(line.get('xAxis'))
+        series.append({"name" : line.get('status_code'), "value" : line.get('codecount')})
+
+    return JsonResponse({'code' : 200, 'result' : {'xAxis' : xAxis, 'series' : series}})

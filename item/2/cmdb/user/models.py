d0872101bf09349b9ba32bb119332b1dca408d82
wangyuxi <2379797392@qq.com>
2019-07-29 18:06:40 +0800

    7/29-2

--- item/2/cmdb/user/models.py
+++ item/2/cmdb/user/models.py
@@ -1,3 +1,13 @@
+# encoding: utf-8
+import json
 from django.db import models
 
-# Create your models here.
+DATA_FILE = 'user.data.json'
+
+
+def get_users():
+    f = open(DATA_FILE, 'rt')
+    users = json.loads(f.read())
+    f.close()
+
+    return users
--- item/2/cmdb/user/views.py
+++ item/2/cmdb/user/views.py
@@ -1,10 +1,15 @@
+# encoding: utf-8
+
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+
+from .models import get_users
+
 import time
 
 def index(request):
-    return  render(request, 'user/index.html', {'current_time': time.time()})
+    return  render(request, 'user/index.html', {
+                'current_time': time.time(), 
+                'users' : get_users()
+                })
    # return HttpResponse('我的第一个网页')

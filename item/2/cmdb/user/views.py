4441d8ca582dc6532b112a3af037894781c0fa59
wangyuxi <2379797392@qq.com>
2019-07-30 17:58:48 +0800

    7/30

--- item/2/cmdb/user/views.py
+++ item/2/cmdb/user/views.py
@@ -1,15 +1,64 @@
 # encoding: utf-8
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
-from .models import get_users
-
-import time
+from .models import get_users,valid_user,delete_user,get_user,valid_update_user,update_user
 
 def index(request):
+    if not request.session.get('user'):
+        return redirect('user:login')
+
     return  render(request, 'user/index.html', {
-                'current_time': time.time(), 
-                'users' : get_users()
-                })
-   # return HttpResponse('我的第一个网页')
+                    'users' : get_users()
+                    })
+
+
+def login(request):
+    if 'GET' == request.method:
+        return render(request, 'user/login.html')
+    else:
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        user = valid_user(name, password)
+        if user:
+            request.session['user'] = user
+            return redirect('user:index')
+        else:
+            return render(request, 'user/login.html', {'name': name, 'errors': {'default':'用户名或者密码错误'}})
+
+
+def logout(request):
+    request.session.flush()
+    return redirect('user:login')
+
+
+def delete(request):
+    if not request.session.get('user'):
+        return redirect('user:login')
+
+    uid = request.GET.get('uid', '')
+    delete_user(uid)
+
+    return redirect('user:index')
+
+def view(request):
+    if not request.session.get('user'):
+        return redirect('user:login')
+
+    uid = request.GET.get('uid', '')
+    user = get_user(uid)
+
+    return  render(request, 'user/view.html', {'user': user})
+
+
+def update(request):
+    if not request.session.get('user'):
+        return redirect('user:login')
+
+    is_valid, user, errors = valid_update_user(request.POST)
+    if is_valid:
+        update_user(user)
+        return redirect('user:index')
+    else:
+        return  render(request, 'user/view.html', {'user': user, 'errors' : errors})

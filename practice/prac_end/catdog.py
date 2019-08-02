695e73e74f5c93cb5a5d0e9590f8b4320fa09889
wangyuxi <2379797392@qq.com>
2019-08-02 13:27:48 +0800

    8/2/1

---
+++ practice/prac_end/catdog.py
@@ -0,0 +1,55 @@
+#encoding: utf-8
+
+import random
+class Dog(object):
+    def __init__(self, name, blood=100):
+        self.name = name
+        self.blood = blood
+
+    def get_blood(self):
+        return self.blood
+
+    def drop_blood(self, drop):
+        self.blood -= drop
+
+    def attack(self, rival):
+        drop = random.randint(0,20)
+        rival.drop_blood(drop)
+        return drop
+    
+
+class Cat(object):
+    def __init__(self, name, blood=100):
+        self.name = name
+        self.blood = blood
+
+    def get_blood(self):
+        return self.blood
+
+    def drop_blood(self, drop):
+        self.blood -= drop
+
+    def attack(self, rival):
+        drop = random.randint(0,20)
+        rival.drop_blood(drop)
+
+        return drop
+
+wangwang = Dog('wangwang')
+
+miaomiao = Cat('miaomiao', 120)
+
+while True:
+    lost_blood = wangwang.attack(miaomiao)
+    free_blood = miaomiao.get_blood()
+    print('wangwang 发起了攻击: miaomiao 失去了 {0} 生命值，还剩 {1} 生命值'.format(lost_blood, free_blood))
+    if miaomiao.get_blood() <= 0:
+        print('wangwang win')
+        break
+
+    lost_blood = miaomiao.attack(wangwang)
+    free_blood = wangwang.get_blood()
+    print('miaomiao 发起了攻击: wangwang 失去了 {0} 生命值，还剩 {1} 生命值'.format(lost_blood, free_blood))
+    if wangwang.get_blood() <= 0:
+        print('miaomiao win')
+        break
